dd4dcebef823eb2647714ddb163208b6da7f8496
See Loo Jane <muigek34@yahoo.com.sg>
2018-07-12 10:42:12 +0800

    refine test code for queueService

--- test/queueService.test.js
+++ test/queueService.test.js
@@ -1,12 +1,48 @@
-const generateQueue = require("../src/queueService");
-const math = require("mathjs");
-const stubRandom = 5;
-math.randomInt = jest.fn(() => stubRandom);
+const mockRandomInt = jest.fn();
+// const generateQueue = require("../src/queueService");
+// math.randomInt = mockRandomInt;
 
-test.only("generateQueue should call math.randomInt, and return an array of integers with that length", () => {
+let generateQueue;
+
+beforeEach(() => {
+    jest.doMock("mathjs", () => {
+        return {
+            randomInt: mockRandomInt
+        };
+    });
+    generateQueue = require("../src/queueService");
+});
+
+test("generateQueue should return an array of integers with that length", () => {
+	// mockRandomInt.mockImplementationOnce(jest.fn(() => 5));
+	// mockRandomInt.mockImplementation(jest.fn(() => 1));
+	mockRandomInt.mockReturnValueOnce(5);
+	mockRandomInt.mockReturnValue(1);
+
 	let result = generateQueue();
 
 	expect(Array.isArray(result)).toBe(true);
-	expect(typeof result[0]).toBe("number");
-	expect(result.length).toBe(stubRandom);
+	result.forEach(num => {
+		expect(num).toBe(1);
+	});
+	expect(result.length).toBe(5);
+});
+
+test("generateQueue should return an array of length between 1 to 10", () => {
+    jest.dontMock("mathjs");
+    generateQueue = require("../src/queueService");
+    
+	let result = generateQueue();
+	console.log(result);
+	expect(result.length).toBeGreaterThanOrEqual(1);
+	expect(result.length).toBeLessThanOrEqual(10);
+});
+
+test("generateQueue should return an array that contains numbers between -20 to 50", () => {
+	let result = generateQueue();
+	result.forEach(num => {
+		console.log(num);
+		expect(num).toBeGreaterThanOrEqual(-20);
+		expect(num).toBeLessThanOrEqual(50);
+	});
 });
